03af806595b3640b5e37380bcae5bfdf33cdeb04
Ari Utomo [Home] <robertus.ari.utomo@gmail.com>
2022-01-30 12:57:56 +0700

    Add menu and text

---
+++ Assets/Games/Scripts/TitleColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TitleColorChanger : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private Color whiteSideColor;
+    [SerializeField] private Color blackSideColor;
+
+    public void UpdateToWhiteSide()
+    {
+        titleText.color = whiteSideColor;
+    }
+
+    public void UpdateToBlackSide()
+    {
+        titleText.color = blackSideColor;
+    }
+}
